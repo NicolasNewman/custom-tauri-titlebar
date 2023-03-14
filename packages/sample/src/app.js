73537df31df9d45567e2dcf311454d83d1e52545
NicolasNewman <nnewman316@gmail.com>
2023-03-14 17:14:07 -0500

    feat(sample): added more examples to the demo app

--- packages/sample/src/app.js
+++ packages/sample/src/app.js
@@ -1,6 +1,10 @@
 import Titlebar from 'custom-tauri-titlebar';
 
-const tb = new Titlebar({background: '#aaaaaa', color: '#ffffff'});
-tb.addButton('close-button', {type: 'html', data: '<p>A</p>'}, () => console.log('close!'), 'start');
-tb.addButton('close-button', {type: 'src', data: 'https://api.iconify.design/mdi:close.svg'}, () => console.log('close!'));
-tb.addButton('close-button', {type: 'html', data: '<p>A</p>'}, () => console.log('close!'));
+const tb = new Titlebar({ background: '#aaaaaa', color: '#ffffff' });
+tb.addTitle('My Application');
+tb.addIcon({ type: 'src', data: 'https://api.iconify.design/ph:globe-hemisphere-west-bold.svg' });
+tb.addButton('close-button', { type: 'html', data: '<p>A</p>' }, () => console.log('close!'), 'start');
+tb.addButton('close-button', { type: 'src', data: 'https://api.iconify.design/mdi:close.svg' }, () =>
+	console.log('close!'),
+);
+tb.addButton('close-button', { type: 'html', data: '<p>A</p>' }, () => console.log('close!'));
