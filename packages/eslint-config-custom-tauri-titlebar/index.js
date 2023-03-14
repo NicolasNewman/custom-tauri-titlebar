a0983d180b4dd8818aee72e5f0168367ebf5964f
NicolasNewman <nnewman316@gmail.com>
2023-03-14 14:30:46 -0500

    feat(linting): added prettier to linting process

--- packages/eslint-config-custom-tauri-titlebar/index.js
+++ packages/eslint-config-custom-tauri-titlebar/index.js
@@ -1,15 +1,16 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  env: {
-    browser: true,
-    es2021: true
-  },
-  plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],  overrides: [],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  rules: {}
-}
+	root: true,
+	parser: '@typescript-eslint/parser',
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	plugins: ['@typescript-eslint'],
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	rules: {},
+};
