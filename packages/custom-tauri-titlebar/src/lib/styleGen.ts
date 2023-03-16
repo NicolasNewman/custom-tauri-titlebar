31a4eae62d8bba086eb5b423ec11a0cad5b07291
NicolasNewman <nnewman316@gmail.com>
2023-03-16 13:25:25 -0500

    feat(titlebar): added helper library for creating styles

---
+++ packages/custom-tauri-titlebar/src/lib/styleGen.ts
@@ -0,0 +1,99 @@
+import { TitleBarOptions } from '../types/titlebar';
+import { lighten } from './color';
+
+export function styleGen(config: TitleBarOptions) {
+	/*========================================*
+	 *              Root Styles               *
+	 *========================================*/
+	const titlebarStyles = `
+    .${config.className} {
+		height: ${config.height}px;
+		background: ${config.theme.bgPrimary};
+		color: ${config.theme.fontPrimary};
+		user-select: none;
+		display: flex;
+		justify-content: space-between;
+		position: fixed;
+		top: 0;
+		left: 0;
+		right: 0;
+	}`;
+
+	/*========================================*
+	 *             Section Styles             *
+	 *========================================*/
+	const sectionStyles = `
+    .${config.className}-section {
+		display: flex;
+		align-items: center;
+	}`;
+
+	/*========================================*
+	 *             Icon Styles                *
+	 *========================================*/
+	const iconStyles = `
+    .${config.className}-icon {
+        height: ${config.height}px;
+        width: ${config.height}px;
+    }
+
+    .${config.className}-icon img {
+        height: calc(${config.height}px - 6px);
+        width: calc(${config.height}px - 6px);
+        padding: 3px;
+    }
+    `;
+
+	/*========================================*
+	 *           Menu/Button Styles           *
+	 *========================================*/
+
+	const sharedButtonStyles = `
+    .${config.className}-button,
+	.${config.className}-menu {
+		display: inline-flex;
+		justify-content: center;
+		align-items: center;
+		height: ${config.height}px;
+	}
+    
+    .${config.className}-button:hover,
+	.${config.className}-menu:hover {
+		background: ${lighten(config.theme.bgPrimary, -0.1)};
+	}
+	
+	.${config.className}-button:active,
+	.${config.className}-menu:active {
+		background: ${lighten(config.theme.bgPrimary, 0.1)};
+	}`;
+
+	const buttonStyles = `
+    .${config.className}-button {
+		width: ${config.height}px;
+	}`;
+
+	const menuStyles = `
+    .${config.className}-menu {
+		padding: 0 6px;
+	}`;
+
+	/*========================================*
+	 *           Bootstrap Override           *
+	 *========================================*/
+	const bootstrapOverride = `
+    .${config.className} div.dropdown {
+        padding: 0 8px;
+    }
+    `;
+
+	return `
+    <style>
+    ${titlebarStyles}
+    ${sectionStyles}
+    ${iconStyles}
+    ${sharedButtonStyles}
+    ${buttonStyles}
+    ${menuStyles}
+    ${bootstrapOverride}
+    </style>`;
+}
