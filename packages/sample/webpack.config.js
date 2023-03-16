1c26e5e168ee4e8d7729d29fdc0e94bbc248ff47
NicolasNewman <nnewman316@gmail.com>
2023-03-16 13:24:21 -0500

    build(sample): added style-loader to demo

--- packages/sample/webpack.config.js
+++ packages/sample/webpack.config.js
@@ -1,19 +1,27 @@
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: {
-    main: path.resolve(__dirname, "./src/app.js"),
-  },
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  plugins: [new HtmlWebpackPlugin({}), new CleanWebpackPlugin()],
-  watchOptions: {
-    ignored: /node_modules([\\]+|\/)+(?!custom-tauri-titlebar)/, 
-  }
+	entry: {
+		main: path.resolve(__dirname, './src/app.js'),
+	},
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	plugins: [new HtmlWebpackPlugin({}), new CleanWebpackPlugin()],
+	watchOptions: {
+		ignored: /node_modules([\\]+|\/)+(?!custom-tauri-titlebar)/,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+		],
+	},
 };
