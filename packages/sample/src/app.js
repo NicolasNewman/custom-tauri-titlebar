5d7e11e2d40713dfd518ac4474c525c20236cd56
NicolasNewman <nnewman316@gmail.com>
2023-03-17 15:23:03 -0500

    feat(sample): updated sample to include menu

--- packages/sample/src/app.js
+++ packages/sample/src/app.js
@@ -1,8 +1,41 @@
 import Titlebar from 'custom-tauri-titlebar';
 
-const tb = new Titlebar({ background: '#aaaaaa', color: '#ffffff' });
+const tb = new Titlebar({});
 tb.addTitle('My Application');
 tb.addIcon({ type: 'src', data: 'https://api.iconify.design/ph:globe-hemisphere-west-bold.svg' });
+tb.addMenu(
+	{
+		label: 'File',
+		items: [
+			{ type: 'item', label: 'Open' },
+			{ type: 'item', label: 'Save' },
+			{ type: 'divider' },
+			{
+				type: 'submenu',
+				submenu: {
+					label: 'Submenu',
+					items: [
+						{ type: 'item', label: 'Item A' },
+						{ type: 'item', label: 'Item B' },
+						{
+							type: 'submenu',
+							submenu: {
+								label: 'Submenu 2',
+								items: [
+									{ type: 'item', label: 'Item C' },
+									{ type: 'item', label: 'Item D' },
+								],
+							},
+						},
+					],
+				},
+			},
+			{ type: 'item', label: 'Close' },
+		],
+	},
+	'start',
+);
+// eslint-disable-next-line @typescript-eslint/no-empty-function
 tb.addButton('close-button', { type: 'html', data: '<p>A</p>' }, () => console.log('close!'), 'start');
 tb.addButton('close-button', { type: 'src', data: 'https://api.iconify.design/mdi:close.svg' }, () =>
 	console.log('close!'),
